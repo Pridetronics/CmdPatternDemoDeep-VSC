25477c162234781d3347f283ebc609ad877622d3
tcjinaz <tcjinaz@gmail.com>
2018-09-15 14:29:21 -0700

    better names

--- src/main/java/tim/cmddemo/DaLoop.java
+++ src/main/java/tim/cmddemo/DaLoop.java
@@ -4,72 +4,85 @@ import java.util.ArrayDeque;
 import java.util.Stack;
 
 public class DaLoop {
+  private static final int TIMEPRINT = 100;
   private ArrayDeque<CommandAbstract> commands;
   private boolean teleop;
   private double fpgaTime;
   private static final double LOOPTIMESTEP = 0.020;
   private static final double ENDTIME = 1.0;
-  
-  public DaLoop() {  // ctor
+
+  public DaLoop() { // ctor
     commands = new ArrayDeque<CommandAbstract>();
-    commands.addFirst( new CommandOne("default") );
+    commands.addFirst(new CommandOne("default"));
     teleop = false;
     fpgaTime = 0.0;
   }
 
-  public void addCommand( CommandAbstract c ) {    // add a command to commands
+  public double getTime() {
+    return fpgaTime;
+  }
+
+  public void addCommand(CommandAbstract c) { // add a command to commands
     commands.add(c);
     c.initialize();
   }
-  public void removeCommand( CommandAbstract c ) { // remove command from the command list
-      c.end(); commands.remove(c);
+
+  public void removeCommand(CommandAbstract c) { // remove command from the command list
+    c.end();
+    commands.remove(c);
   }
-  public boolean toggleTeleop() {                // toggle teleop on/off
+
+  public boolean toggleTeleop() { // toggle teleop on/off
     return teleop = !teleop;
   }
 
-  public boolean runRobot (){
+  public boolean runRobot() {
     fpgaTime = 0;
-    while(teleop) {
+    while (teleop) {
       fpgaTime += LOOPTIMESTEP;
-      if ( fpgaTime > ENDTIME ) { toggleTeleop(); }
+      if (fpgaTime > ENDTIME) {
+        toggleTeleop();
+      }
       teleopPeriodic();
     }
     return true;
   }
-  
+
   public boolean teleopPeriodic() {
     run();
     return true;
   }
-  
+
   public boolean run() {
     boolean status = true;
     Stack<CommandAbstract> deleteList = new Stack<>();
 
-    //    if ( (fpgaTime*1000.0) % 1 == 0 ) {
-      System.out.println(); System.out.println("Time " + fpgaTime);
-//    }
-    for ( CommandAbstract c : commands ) {
-      if ( c.getName() == "default" ) continue;  // skip the defuka command
+    if ((fpgaTime * 1000.0) % TIMEPRINT == 0) {
+      System.out.println();
+      System.out.println("Time " + fpgaTime);
+    }
+    for (CommandAbstract c : commands) {
+      if (c.getName() == "default")
+        continue; // skip the default ommand
       c.execute();
-      if ( c.isFinished() ) {
+      if (c.isFinished()) {
         deleteList.push(c);
       }
     }
-    while ( ! deleteList.empty()) {
+    while (!deleteList.empty()) {
       commands.remove(deleteList.pop());
     }
     return status;
-  }  // end run
+  } // end run
 
-  public static void main(String[] args){
-    DaLoop dl = new DaLoop();
-    dl.addCommand(new CommandOne("A"));
-    dl.addCommand(new CommandTwo("B"));
-    dl.addCommand(new CommandThree("C"));
-    dl.addCommand(new CommandFour("D"));
-    dl.toggleTeleop();
-    dl.runRobot();
-  }  //  end main
-}  // end class DaLoop
+  public static void main(String[] args) {
+    DaLoop daRobot = new DaLoop();
+    daRobot.addCommand(new CommandOne( "A" ));
+    daRobot.addCommand(new CommandTwo( "B" );
+    daRobot.addCommand(new CommandThree( "C" ));
+    daRobot.addCommand(new CommandFour( "D" ));
+    daRobot.toggleTeleop();
+    daRobot.runRobot();
+    System.out.println( Time " + daRobot.getTime() );
+  } // end main
+} // end class DaLoop
